244be79691f1e1a5f20202bd9060f9d199d91183
Zepar99 <pratikkborse@gmail.com>
2021-11-09 11:51:33 +0530

    movement

--- 2d game/Assets/scripts/PlayerController.cs
+++ 2d game/Assets/scripts/PlayerController.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float playerspeed;
+    public float jump;
+    public float jumpForce;
+    private Rigidbody2D rd;
     public Animator animator;
+    bool Grounded;
     private void Awake()
     {
       Debug.Log("Player controller awake");
+      rd = gameObject.GetComponent<Rigidbody2D>();
+      SpriteRenderer sprite = gameObject.GetComponent<SpriteRenderer>();
     }
 //    private void OnCollisionEnter2D(Collision2D collision)
 //    {
@@ -16,7 +23,25 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         float speed = Input.GetAxisRaw("Horizontal");
-        animator.SetFloat("speed",Mathf.Abs(speed));
+        float speedv = Input.GetAxisRaw("Jump");
+        
+        MoveCharater(speed,speedv);
+        PlayerMovement(speed,speedv);
+    }
+      private void MoveCharater(float speed,float speedv)
+      {
+          Vector3 position = transform.position;
+          position.x += speed  * playerspeed * Time.deltaTime;
+          transform.position = position;
+
+          if(speedv>0)
+          {
+              rd.AddForce(new Vector2(0f,jump),ForceMode2D.Force);
+          }
+      }
+        private void PlayerMovement(float speed,float speedv)
+    {
+         animator.SetFloat("speed",Mathf.Abs(speed));
 
         Vector3 scale =  transform.localScale;
         if(speed<0)
@@ -27,6 +52,16 @@ public class PlayerController : MonoBehaviour
            scale.x =Mathf.Abs(scale.x);
 
         }
+
         transform.localScale = scale;
+        if(speedv>0)
+        {
+        animator.SetBool("Jump",true);
+        }
+        else
+        {
+          animator.SetBool("Jump",false);
+        }
+    
     }
 }
